a689da7767d76565735056ccc8ee1c03597b6f91
lvshuangyong <1721446288@qq.com>
2020-09-16 00:20:22 +0800

    修改jwt校验方式

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,68 +1,74 @@
 <template>
-  <header :class="{Login: isLogin, 'no-login': !isLogin}">
-    <template v-if="!isLogin">
-    <h1><router-link to="/">Let's share</router-link></h1>
-    <p>精品博客汇聚</p>
+<header :class="{Login: isLogin, 'no-login': !isLogin}">
+  <template v-if="!isLogin">
+    <h1>
+      <router-link to="/">BLOG CENTER</router-link>
+    </h1>
+    <p>知识分享中心</p>
     <div class="btns">
-    <router-link to="/login"><el-button >立即登录</el-button></router-link>
-    <router-link to="/register"><el-button>注册账号</el-button></router-link>
+      <router-link to="/login">
+        <el-button>立即登录</el-button>
+      </router-link>
+      <router-link to="/register">
+        <el-button>注册账号</el-button>
+      </router-link>
     </div>
-    </template>
-    <template v-if="isLogin">
-      <h1><router-link to="/">Let's share</router-link></h1>
-      <router-link to="/create"><i class="edit el-icon-plus"></i></router-link>
-      <div class="user">
-        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username">
-        <ul>
-          <li><router-link to="/my">我的</router-link></li>
-          <li><a href="#" @click="onLogout">注销</a></li>
-        </ul>
-      </div> 
-    </template>
-    </header>
+  </template>
+  <template v-if="isLogin">
+    <h1>
+      <router-link to="/">BLOG CENTER</router-link>
+    </h1>
+    <router-link to="/create">
+      <i class="edit el-icon-circle-plus-outline"></i>
+    </router-link>
+    <div class="user">
+      <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username" />
+      <ul>
+        <li>
+          <router-link to="/my">我的</router-link>
+        </li>
+        <li>
+          <a href="#" @click="onLogout">注销</a>
+        </li>
+      </ul>
+    </div>
+  </template>
+</header>
 </template>
 
 <script>
+import auth from "@/api/auth";
 
-import auth from '@/api/auth'
+window.auth = auth;
 
-window.auth = auth
-
-import { mapGetters, mapActions } from 'vuex'
+import {
+  mapGetters,
+  mapActions
+} from "vuex";
 
 export default {
   data() {
-    return {}
+    return {};
   },
 
   computed: {
-    ...mapGetters([
-      'isLogin',
-      'user'
-    ])
+    ...mapGetters(["isLogin", "user"]),
   },
 
   created() {
-    this.checkLogin()
+    this.checkLogin();
   },
 
   methods: {
-    ...mapActions([
-      'checkLogin',
-      'logout'
-    ]),
+    ...mapActions(["checkLogin", "logout"]),
     onLogout() {
-    this.logout()
-  }
-  }
-
-
-}
+      this.logout();
+    },
+  },
+};
 </script>
 
-
 <style lang="less">
-
 @import "../assets/base.less";
 
 header.no-login {
@@ -76,10 +82,11 @@ header.no-login {
     color: #fff;
     margin: 60px 0 0 0;
     text-transform: uppercase;
-      a {
+
+    a {
       text-decoration: none;
       color: #fff;
-      }
+    }
   }
 
   p {
@@ -90,6 +97,7 @@ header.no-login {
   .btns {
     margin-top: 20px;
   }
+
   button {
     margin: 20px 5px 0;
   }
@@ -108,7 +116,7 @@ header.Login {
     text-transform: uppercase;
     flex: 1;
 
-      a {
+    a {
       text-decoration: none;
       color: #fff;
     }
@@ -127,7 +135,7 @@ header.Login {
     margin-left: 15px;
   }
 
-    .user {
+  .user {
     position: relative;
 
     ul {
@@ -136,7 +144,7 @@ header.Login {
       right: 0;
       list-style: none;
       border: 1px solid #eaeaea;
-      margin:0;
+      margin: 0;
       padding: 0;
       background-color: #fff;
 
@@ -151,7 +159,6 @@ header.Login {
           background-color: #eaeaea;
         }
       }
-
     }
 
     &:hover ul {
@@ -159,5 +166,4 @@ header.Login {
     }
   }
 }
-
 </style>
